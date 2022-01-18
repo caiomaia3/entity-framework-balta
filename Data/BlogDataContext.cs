0d908cfb9ab2c86fad57b641e6908327e34df44b
caiomaia3 <caiomaia3@gmail.com>
2022-01-18 00:05:18 -0300

    Db Contextex configuration

--- Data/BlogDataContext.cs
+++ Data/BlogDataContext.cs
@@ -2,11 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Data
 {
     public class BlogDataContext : DbContext
     {
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<Post> Posts { get; set; } = null!;
+        public DbSet<PostTag> PostTags { get; set; } = null!;
+        public DbSet<Role> Roles { get; set; } = null!;
+        public DbSet<Tag> Tags { get; set; } = null!;
+        public DbSet<User> Users { get; set; } = null!;
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlServer("Server=localhost,1433;Database=blog;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True");
     }
 }
