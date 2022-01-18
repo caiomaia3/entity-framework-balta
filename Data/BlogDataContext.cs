94855b26f3cea26eec900e5ec50e87b5089ac3a7
caiomaia3 <caiomaia3@gmail.com>
2022-01-18 00:05:18 -0300

    Db Contextex configuration

--- Data/BlogDataContext.cs
+++ Data/BlogDataContext.cs
@@ -2,11 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Data
 {
     public class BlogDataContext : DbContext
     {
-
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<Post> Posts { get; set; } = null!;
+        public DbSet<PostTag> PostTags { get; set; } = null!;
+        public DbSet<Role> Roles { get; set; } = null!;
+        public DbSet<Tag> Tags { get; set; } = null!;
+        public DbSet<User> Users { get; set; } = null!;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Blog.Data;
+
 Console.WriteLine("Hello, World!");
+BlogDataContext ctx = new();
