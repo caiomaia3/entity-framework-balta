54330bdcd87b6611a93a0601c3f609d2fd760be3
caiomaia3 <caiomaia3@gmail.com>
2022-01-20 14:39:00 -0300

    include update tag

--- Program.cs
+++ Program.cs
@@ -1,15 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// using System.Linq;
 using Blog.Data;
 using Blog.Models;
 
 
 using (var context = new BlogDataContext())
 {
-    var tag = new Tag { Name = "ASPNET", Slug = "aspnetCaio" };
-    context.Tags.Add(tag);
+    // var tag = new Tag { Name = "ASPNET", Slug = "aspnetCaio" };
+    // context.Tags.Add(tag);
+    // context.SaveChanges();
+
+    var tag = context.Tags.FirstOrDefault<Tag>(x => x.Id == 3);
+    tag.Name = ".NET";
+    tag.Slug = "dotnet";
+
+    context.Update(tag);
     context.SaveChanges();
-
-
 }
 Console.WriteLine("Programa finalizado.");
