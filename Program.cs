6a47ea1c8955a4a8e6c8cbf5f21267f8b3fe7d64
caiomaia3 <caiomaia3@gmail.com>
2022-01-20 16:56:09 -0300

    section-2 first commit

--- Program.cs
+++ Program.cs
@@ -1,24 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Blog.Data;
 using Blog.Models;
-
+using Microsoft.EntityFrameworkCore;
 
 using (var context = new BlogDataContext())
 {
-    // var tag = new Tag { Name = "ASPNET", Slug = "aspnetCaio" };
-    // context.Tags.Add(tag);
-    // context.SaveChanges();
-
-    // var tag = context.Tags.FirstOrDefault<Tag>(x => x.Id == 3);
-    // tag.Name = ".NET";
-    // tag.Slug = "dotnet";
-
-    // context.Update(tag);
-    // context.SaveChanges();
-
-    var tag = context.Tags.FirstOrDefault<Tag>(x => x.Id == 3);
-
-    context.Remove(tag);
-    context.SaveChanges();
 }
 Console.WriteLine("Programa finalizado.");
