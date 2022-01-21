bf9785215594de32a7cfb6f16e3b21dbebae829d
caiomaia3 <caiomaia3@gmail.com>
2022-01-21 17:24:34 -0300

    one2many many2many ralation

--- Models/Category.cs
+++ Models/Category.cs
@@ -9,7 +9,7 @@ namespace Blog.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Slug { get; set; }
-        // public List<Post> Posts { get; set; }
+        public List<Post> Posts { get; set; }
 
         // public Category() => Posts = new List<Post>();
     }
